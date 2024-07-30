d8416c5d3a4156801e770760c00ebd7aeccf13cb
Raymond Chong <mathsgod@yahoo.com>
2024-07-30 17:19:12 +0800

    ✨ 新增(ChatCompletions.php)：實作createStream方法以支援流式請求
    這次的變更引入了`createStream`方法，該方法使用React HTTP客戶端發送流式請求至OpenAI的聊天完成API。這樣的改動使得應用能夠即時接收和處理來自API的數據流，提升了用戶體驗和應用的反應速度。

--- src/ChatCompletions.php
+++ src/ChatCompletions.php
@@ -2,6 +2,10 @@
 
 namespace OpenAI;
 
+use Psr\Http\Message\ResponseInterface;
+use React\Stream\ReadableStreamInterface;
+use React\Stream\ThroughStream;
+
 class ChatCompletions
 {
     public $client;
@@ -24,5 +28,54 @@ class ChatCompletions
             "json" => $options,
             "stream" => $stream
         ]);
+    }
+
+    public function createStream(array $body)
+    {
+        $browser = new \React\Http\Browser();
+        $body["stream"] = true;
+        $promise = $browser->requestStreaming("POST", $this->client->base_url . "chat/completions", [
+            "Authorization" => "Bearer " . $this->client->openai_api_key,
+            "Content-Type" => "application/json",
+        ], json_encode($body, JSON_UNESCAPED_UNICODE));
+
+        $stream = new ThroughStream();
+
+        $promise->then(function (ResponseInterface $response) use (&$stream) {
+            $s = $response->getBody();
+            assert($s instanceof ReadableStreamInterface);
+
+            $next_chunk = "";
+
+            $s->on("data", function ($chunk) use (&$stream, &$next_chunk) {
+
+
+                $chunk = $next_chunk . $chunk;
+                $lines = explode("\n\n", $chunk);
+
+                //if last line is not empty, then it is not complete, only process the lines without last line
+                if ($lines[count($lines) - 1]) {
+                    $next_chunk = array_pop($lines);
+                } else {
+                    $next_chunk = "";
+                }
+
+                //filter out empty lines
+                $lines = array_filter($lines);
+
+                foreach ($lines as $line) {
+                    $stream->write($line);
+                }
+            });
+
+            $s->on("end", function () use (&$stream) {
+                $stream->end();
+            });
+
+            $s->on("close", function () use (&$stream) {
+                $stream->close();
+            });
+        });
+        return $stream;
     }
 }
