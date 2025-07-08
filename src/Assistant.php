d29bdbc209f604b3683fa80a88dfab63417a9155
Raymond Chong <mathsgod@yahoo.com>
2025-07-08 12:48:30 +0800

    ✨ 更新(Assistant): 將`__construct`、`modify`和`delete`方法標記為過時
    * 將`__construct`方法標記為過時，提醒使用者未來的變更。
    * 將`modify`和`delete`方法標記為過時，建議使用替代方案。

--- src/Assistant.php
+++ src/Assistant.php
@@ -7,12 +7,18 @@ class Assistant
     private $client;
     public $assistant_id;
 
+    /**
+     * @deprecated This constructor is deprecated.
+     */
     public function __construct(Client $client, string $assistant_id)
     {
         $this->client = $client;
         $this->assistant_id = $assistant_id;
     }
 
+    /**
+     * @deprecated This method is deprecated.
+     */
     public function modify(array $body)
     {
         return $this->client->post("assistants/" . $this->assistant_id, [
@@ -23,6 +29,9 @@ class Assistant
         ]);
     }
 
+    /**
+     * @deprecated This method is deprecated.
+     */
     public function delete()
     {
         return $this->client->delete("assistants/" . $this->assistant_id, [
